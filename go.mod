c1a624dcb8a5578aea22af75aafbf98bd48a7c87
Miran Kurukulasuriya <miranlfk@gmail.com>
2024-05-24 16:06:56 +0530

    Basic rabbitmq producer/consumer message queue service

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Miranlfk/Go-RabbitMQ
 
 go 1.20
+
+require github.com/rabbitmq/amqp091-go v1.10.0
--- send/send.go
+++ send/send.go
@@ -1,1 +1,62 @@
-package main 
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	queue, err := ch.QueueDeclare(
+		"Hello",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	// Prompt the user to submit a message
+	fmt.Print("Enter the message to send: ")
+	reader := bufio.NewReader(os.Stdin)
+	body, err := reader.ReadString('\n')
+	failOnError(err, "Failed to read input")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = ch.PublishWithContext(
+		ctx, 
+		"", 
+		queue.Name, 
+		false, 
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		},
+	)
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s", body)
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
